d8448fdbb691eb029aea79830fd772f569b1ffba
Ben Tierney <bentierney5@gmail.com>
2022-06-17 16:29:28 +0100

    Update fibonacciCalc.py

--- fibonacciCalc.py
+++ fibonacciCalc.py
@@ -1,2 +1,16 @@
 # Fibonacci Calculation Algorithm
-# Last updated 08 June 2022
+# Last updated 17 June 2022
+
+# Non-recursive solution to calculate nth value for Fibonacci sequence 
+
+def fibonacci(n):
+     
+    # taking 1st two fibonacci numbers as 0 and 1
+    f = [0, 1]
+     
+     
+    for i in range(2, n+1):
+        f.append(f[i-1] + f[i-2])
+    return f[n]
+     
+print(fibonacci(9))
